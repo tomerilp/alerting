793d0fb0dd9493a1947ae818f9668261aa3a69b1
David Chauvière <david.chauviere@orange.com>
2019-05-27 23:21:46 +0200

    Add Mail destination   * Support SSL/TLS   * Authentification   * Message's body is html by dedault

--- alerting/src/test/kotlin/com/amazon/opendistroforelasticsearch/alerting/AlertingRestTestCase.kt
+++ alerting/src/test/kotlin/com/amazon/opendistroforelasticsearch/alerting/AlertingRestTestCase.kt
@@ -109,7 +109,8 @@ abstract class AlertingRestTestCase : ESRestTestCase() {
                 lastUpdateTime = Instant.now(),
                 chime = null,
                 slack = null,
-                customWebhook = null)
+                customWebhook = null,
+                mail = null)
     }
 
     protected fun verifyIndexSchemaVersion(index: String, expectedVersion: Int) {
